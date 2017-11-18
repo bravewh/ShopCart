c913bedaab749c92f558e1c41df7dec678cb6c56
weihao <1120575939@qq.com>
2017-11-18 12:07:02 +0800

    提交购物车勾选逻辑

--- app/src/main/java/com/example/wh/cart/ListCartAdapter.java
+++ app/src/main/java/com/example/wh/cart/ListCartAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,9 @@ public class ListCartAdapter extends ArrayAdapter {
     private ViewHodler mHodler;
     private View mView;
     private ArrayList<CartList> mCartLists = new ArrayList<>();
+    public static boolean isChecked = true;
+    public static boolean  mChecked1 ;
+
 
     public ListCartAdapter(@NonNull Context context, int resource, @NonNull List objects) {
         super(context, resource, objects);
@@ -33,7 +37,6 @@ public class ListCartAdapter extends ArrayAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        CartList cartList = mCartLists.get(position);
         if (convertView == null) {
             mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_item, null);
             mHodler = new ViewHodler();
@@ -50,13 +53,56 @@ public class ListCartAdapter extends ArrayAdapter {
             mHodler = (ViewHodler) mView.getTag();
 
         }
-        mHodler = (ViewHodler) mView.getTag();
+
+        CartList cartList = mCartLists.get(position);
+
         mHodler.mCheckBox.setChecked(false);
         mHodler.mImageView.setImageResource(cartList.getImage());
         mHodler.mThingName.setText(cartList.getThingName());
         mHodler.mThingColor.setText(cartList.getThingColor());
         mHodler.mPrice.setText(cartList.getPrice());
         mHodler.mCount.setText(cartList.getCount());
+
+        //勾选商品
+        mHodler.mCheckBox.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+            @Override
+            public void onClick(View view) {
+
+
+                mChecked1 = mHodler.mCheckBox.isChecked();
+                if (!mChecked1) {
+                    Toast.makeText(getContext(), "勾选", Toast.LENGTH_SHORT).show();
+                    mChecked1 = true;
+                } else {
+                    Toast.makeText(getContext(), "未勾选", Toast.LENGTH_SHORT).show();
+                    mChecked1 = false;
+                }
+
+
+/*                //勾选
+                if (isChecked) {
+                    Toast.makeText(getContext(), "勾选了", Toast.LENGTH_SHORT).show();
+
+                        isChecked = false;
+
+                    //不勾选
+                } else {
+
+                    Toast.makeText(getContext(), "没有勾选", Toast.LENGTH_SHORT).show();
+                    isChecked = true;
+                }*/
+            }
+
+
+        });
+
+
         return mView;
 
     }
